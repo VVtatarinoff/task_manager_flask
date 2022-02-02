118262de542d46b333f28a1fdd6a71a52ee78af6
Vladimir <vvtatarinoff@gmail.com>
2022-02-02 17:27:08 +0300

    user list

---
+++ task_manager/auths/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import Email, EqualTo, Length, DataRequired
+
+
+class CreateUser(FlaskForm):
+    email = StringField('Email: ',
+                        validators=[Email(),
+                                    Length(max=100),
+                                    DataRequired()])
+    name = StringField('Name: ',
+                       validators=[Length(max=20),
+                                   DataRequired()])
+    first_name = StringField('First name: ',
+                             validators=[Length(max=70)])
+    last_name = StringField('Last name: ',
+                            validators=[Length(max=70)])
+    psw1 = PasswordField('Enter password: ',
+                         validators=[Length(min=5,max=70),
+                                     DataRequired()])
+    psw2 = PasswordField('Repeat password: ',
+                         validators=[Length(min=5,max=70),
+                                     DataRequired(),
+                                     EqualTo(psw1)])
+    submit = SubmitField('Register')
+
+
+class SignInForm(FlaskForm):
+    email = StringField('Email: ',
+                        validators=[Email(),
+                                    Length(max=100),
+                                    DataRequired()])
+    psw1 = PasswordField('Enter password: ',
+                         validators=[Length(min=5, max=70),
+                                     DataRequired()])
--- task_manager/auths/users.py
+++ task_manager/auths/users.py
@@ -1,6 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-users_bp = Blueprint('users', __name__)
+users_bp = Blueprint('users', __name__, template_folder='templates')
 
 from task_manager.models import Users
 
@@ -13,3 +13,19 @@ def register():
 @users_bp.route('/signin', methods=('POST', 'GET'))
 def sign_in():
     return 'signing in'
+
+
+@users_bp.route('/users')
+def get_user_list():
+    context = dict()
+    context['Title'] = 'Users'
+    context['table_heads'] = ('ID', 'User name',
+                              'Full name', 'Creation date')
+    users = []
+    try:
+        users = Users.query.all()
+    except:
+        print('Db access error')
+
+    context['table_data'] = users
+    return render_template('users/user_list.html', **context)
