a2c1049d9f52a6277ab27d806770845688c686a1
vladimir <vvtatarinoff@gmail.com>
2022-02-09 22:49:14 +0300

    welcome message

--- task_manager/main.py
+++ task_manager/main.py
@@ -17,7 +17,7 @@ main_bp = Blueprint('main', __name__, template_folder='templates')
 
 @main_bp.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 
 def main():
