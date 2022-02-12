1b1622f1a236c56b4f79c87e7837eb096c424020
Vladimir <vvtatarinoff@gmail.com>
2022-02-12 11:57:16 +0300

    bootstrap

--- task_manager/main.py
+++ task_manager/main.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from flask import Blueprint
 
-main_bp = Blueprint('main', __name__, template_folder='templates')
+main_bp = Blueprint('main', __name__,
+                    template_folder='templates',
+                    static_folder='static')
 
 
 @main_bp.route('/')
