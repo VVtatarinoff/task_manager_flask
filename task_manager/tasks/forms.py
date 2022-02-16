236df9465ceac84a3d3b3ec69683eb150cea4b3f
vladimir <vvtatarinoff@gmail.com>
2022-02-16 18:22:29 +0300

    create task template first draft

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,18 +1,32 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField, SelectMultipleField, DateField
 from wtforms.validators import Length, DataRequired, Regexp
 
 
 class CreateTask(FlaskForm):
-    name = StringField('Name: #',
+    task_name = StringField('Name: ',
                        validators=[Length(max=20),
                                    DataRequired(),
                                    Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                           'Name must have only letters, '
                                           'numbers, dots or underscores')])
     description = StringField('Description: ',
-                              validators=[Length(max=200)])
+                              validators=[Length(max=200),
+                                          DataRequired(),])
+    executor = SelectField('Executor ', validators=[DataRequired()])
+    tags = SelectMultipleField('Tags ')
     submit = SubmitField('Create')
+    step_name = SelectField('Step name')
+    start_step_date = DateField('Start date')
+    planned_step_end = DateField('Deadline')
+    add_step = SubmitField('Add step')
+
+    def __init__(self, executors=[], tags=[], step_names=[], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.executor.choices = executors
+        self.tags.choices = tags
+        self.step_name.choices = step_names
+
 
 
 class EditTaskForm(FlaskForm):
--- task_manager/tasks/tasks.py
+++ task_manager/tasks/tasks.py
@@ -1,9 +1,13 @@
 import logging
+from datetime import datetime
 
 from flask import Blueprint, redirect, url_for, request, flash, render_template
 from flask_login import login_required
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import load_only
 
+from task_manager.statuses.models import Status
+from task_manager.tags.models import Tag
 from task_manager.tasks.forms import CreateTask, EditTaskForm
 from task_manager.tasks.models import Task, Plan
 
@@ -42,8 +46,49 @@ def show_task_detail(id):
     return redirect(url_for('main.index'))
 
 
-@tasks_bp.route('/task_create')
+@tasks_bp.route('/task_create', methods=['GET', 'POST'])
 @login_required
 @permission_required(Permission.MANAGE)
 def create_task():
-    return redirect(url_for('main.index'))
+    executors = User.query.filter(User.role_id.in_([1,2])).all()
+    executors_list = [(None, '           ')] + \
+                     list(map(lambda x: (x.id, f"{x.first_name} {x.last_name}"), executors))
+    if len(executors_list)<2:
+        flash('No executors or managers in database ', 'error')
+        return redirect(url_for("tasks.show_tasks_list"))
+    tags = Tag.query.all()
+    tags_list = [(None, '           ')]+list(map(lambda x: (x.id, x.name), tags))
+    statuses = Status.query.all()
+    status_list = [(None, '           ')]+list(map(lambda x: (x.id, x.name), statuses))
+    form = CreateTask(executors=executors_list, tags=tags_list, step_names=status_list)
+    if form.add_step:
+        if msg := get_error_for_step(form):
+            flash(msg)
+        else:
+            pass
+    context = dict()
+    context['form'] = form
+    context['title'] = 'Create task'
+
+    return render_template('tasks/task_creation.html', **context)
+
+def get_error_for_step(form):
+    step_id = form.step_name.data
+    start = form.start_step_date.data
+    end = form.planned_step_end.data
+    if step_id == "None":
+        return "Choose the step to include"
+    if not start:
+        return "Input start date"
+    if not end:
+        return "Input deadline"
+    step_id = int(step_id)
+    step = list(Status.query.filter_by(id=step_id).all())
+    if len(step) == 0:
+        return "No such status exists in database"
+    if end > start:
+        return "Start date less than deadline"
+    today = datetime.today()
+    if start < today:
+        return "The start point is in the past"
+    return False
